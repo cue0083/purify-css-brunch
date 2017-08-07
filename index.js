7753b3c716ec847638f8f64ed4c5046a73895910
Chukwuemeka Ezekwe <2125331+cue0083@users.noreply.github.com>
2017-08-07 17:50:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const purify = require('purify-css');
+const request = require('request-promise-native');
+const tmp = require('tmp-promise');
+const fs = require('fs');
 
 class PurifyCSS {
 	constructor(config) {
@@ -8,8 +11,27 @@ class PurifyCSS {
 	}
   optimize(file) {
   	try {
-  		const purified = purify(this.config.content, file.data, this.config.options);
-    	return Promise.resolve(purified);
+		const options = this.config.options;
+		const promises = this.config.content.map(function (c) {
+			if (c.startsWith('http')) {
+				return request.get(c).then(function(response) {
+					return tmp.file().then(function (tmpFile) {
+						return new Promise(function (resolve, reject) {
+							const writeStream = fs.createWriteStream(tmpFile.path);
+							writeStream.write(response);
+							resolve(tmpFile.path);
+						});
+					})
+				});
+			} else {
+				return Promise.resolve(c);
+			}
+		});
+		return Promise.all(promises).then(function (content) {
+			console.log(content);
+	  		const purified = purify(content, file.data, options);
+			return Promise.resolve(purified);
+		});
   	} catch (error) {
   		return Promise.reject(error);
   	}
